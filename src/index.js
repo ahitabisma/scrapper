241f7fd409254f5834a96d987019d6736985f26e
ahitabisma <ahitabisma21@gmail.com>
2024-05-18 02:09:19 +0700

    favicon

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const { getKabupatenKota, getProvinsi, getKabupatenKotaByProvinsi, provinsiJson } = require("./controller/scrap");
-
+const favicon = require("serve-favicon");
 const app = express();
-
 const port = 3000;
 
+app.use(favicon(path.join(__dirname, "../", "public", "img", "logo.png")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
